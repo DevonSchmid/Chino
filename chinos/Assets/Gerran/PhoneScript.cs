72046313d66da555f3a70f203a50436a15966a6f
gerranR <60340232+gerranR@users.noreply.github.com>
2020-05-14 14:39:20 +0200

    inv half

--- chinos/Assets/Gerran/PhoneScript.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PhoneScript : MonoBehaviour
-{
-    public GameObject image;
-}
---
+++ chinos/Assets/Gerran/ScriptebleObjects/Items/Scripts/ItemObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "new scripteble object", menuName ="inventory System/Item")]
+public class ItemObject : ScriptableObject
+{
+    public GameObject prefab;
+    public int id;
+    public Sprite sprite;
+}
